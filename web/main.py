c8e66eb9f85971365737d3fcbdaf0c87b9a0185f
my113 <my113@duke.edu>
2018-11-27 12:02:45 -0500

    debug

--- web/main.py
+++ web/main.py
@@ -130,7 +130,7 @@ def upload():
 def getRequirement():
     print("enter get requirement")
     data = request.data
-    print("get data"+data)
+    print(data)
     info = initializeDatabase(data)
     print(info)
     return render_template('upload.html')
