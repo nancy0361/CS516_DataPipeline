ad92511bee9e7188b981d463c6d30abef3318176
Naixin Yu <naixin.yu@duke.edu>
2019-01-19 23:24:26 -0500

    add check db existance in status page

--- dataMining/checkDB.py
+++ dataMining/checkDB.py
@@ -1,17 +1,29 @@
 from pymongo import MongoClient
 
 def checkStatus():
-    conn = MongoClient('127.0.0.1', 27017)
-    mydb = conn.testDB
     report = {}
-    report['Database'] = mydb.command("dbstats")
+    conn = MongoClient('127.0.0.1', 27017)
+    
+    dbNames = conn.list_database_names()
+    if 'testDB' not in dbNames:
+        report['error'] = "Oops! The database haven't been created."        
+    
+    mydb = conn.testDB
+    databaseStatus = mydb.command("dbstats")
+    report['Database'] = {}
+    report['Database']['Database Name'] =  databaseStatus['db']
+    report['Database']['Number of Tables'] = databaseStatus['collections']
+    report['Database']['Number of Views'] = databaseStatus['views']
+    report['Database']['Total Number of Records'] = databaseStatus['objects']
+    report['Database']['Used Disk Size'] = str(format(databaseStatus['fsUsedSize'] / 1000000000, '.2f')) + ' GB'
+    report['Database']['Total Disk SIze'] = str(format(databaseStatus['fsTotalSize'] / 1000000000, '.2f')) + ' GB'
     report['Collection'] = {}
     report['Collection']['collection_names'] = mydb.list_collection_names()
     for table in mydb.list_collection_names():
         stats = mydb.command("collstats", table)
         report['Collection'][table] = {}
-        report['Collection'][table]['name'] = stats['ns']
-        report['Collection'][table]['pk'] = '_id'
-        report['Collection'][table]['documents'] = stats['count']
-        report['Collection'][table]['size'] = stats['size'] 
+        report['Collection'][table]['Name'] = stats['ns']
+        report['Collection'][table]['Primary Key'] = '_id'
+        report['Collection'][table]['Records Number'] = stats['count']
+        report['Collection'][table]['Table Size'] = str(stats['size']) + ' Bytes'
     return report
